e9b96ee1fe275088e9928308af65a476ee7aac33
HIRIYURS <santhosh.shankar@gmail.com>
2019-03-25 03:45:32 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,75 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatToolbarModule, MatFormFieldModule, MatInputModule, MatOptionModule, MatSelectModule, MatIconModule, MatButtonModule, MatCardModule, MatTableModule, MatDividerModule, MatSnackBarModule } from '@angular/material';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddtaskComponent } from './components/addtask/addtask.component';
+import { ViewtaskComponent } from './components/viewtask/viewtask.component';
+import { EdittaskComponent } from './components/edittask/edittask.component';
+import { EdituserComponent } from './components/edituser/edituser.component';
+import { EditprojectComponent } from './components/editproject/editproject.component';
+import { ViewuserComponent } from './components/viewuser/viewuser.component';
+import { ViewprojectComponent } from './components/viewproject/viewproject.component';
+import { AdduserComponent } from './components/adduser/adduser.component';
+import { AddprojectComponent } from './components/addproject/addproject.component';
+
+const routes: Routes = [
+  { path: 'addtask', component: AddtaskComponent},
+  { path: 'edittask/:id', component: EdittaskComponent},
+  { path: 'viewtask', component: ViewtaskComponent},
+  { path: 'addproject', component: AddprojectComponent},
+  { path: 'editproject/:id', component: EditprojectComponent},
+  { path: 'viewproject', component: ViewprojectComponent},
+  { path: 'adduser', component: AdduserComponent},
+  { path: 'edituser/:id', component: EdituserComponent},
+  { path: 'viewuser', component: ViewuserComponent},
+  { path: '', redirectTo: 'viewproject', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddtaskComponent,
+    ViewtaskComponent,
+    EdittaskComponent,
+    EdituserComponent,
+    EditprojectComponent,
+    ViewuserComponent,
+    ViewprojectComponent,
+    AdduserComponent,
+    AddprojectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatOptionModule, 
+    MatSelectModule, 
+    MatIconModule, 
+    MatButtonModule, 
+    MatCardModule, 
+    MatTableModule, 
+    MatDividerModule, 
+    MatSnackBarModule,
+    MatSliderModule,
+    MatDatepickerModule,
+    MatMomentDateModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
