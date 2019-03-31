07dc454706fcbd6f0a6169579bf97ac41c5ff87f
Santhosh Shankar <santhosh.shankar@gmail.com>
2019-03-30 19:38:24 -0500

    Fixing CI issues with karma - checkin 8

--- src/app/components/addtask/addtask.component.spec.ts
+++ src/app/components/addtask/addtask.component.spec.ts
@@ -1,4 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { Router } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { MatToolbarModule, MatFormFieldModule, MatInputModule, MatOptionModule, 
          MatSelectModule, MatIconModule, MatButtonModule, MatCardModule, 
          MatTableModule, MatDividerModule, MatSnackBarModule, MatAutocompleteModule,
@@ -7,6 +15,13 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 
 import { AddtaskComponent } from './addtask.component';
+import { ViewtaskComponent } from '../viewtask/viewtask.component';
+
+const routes: Routes = [
+  { path: 'addtask', component: AddtaskComponent},
+  { path: 'viewtask', component: ViewtaskComponent},
+  { path: '', redirectTo: 'viewtask', pathMatch: 'full'}
+];
 
 describe('AddtaskComponent', () => {
   let component: AddtaskComponent;
@@ -15,6 +30,12 @@ describe('AddtaskComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        RouterModule.forRoot(routes),
+        FormsModule,
+        ReactiveFormsModule,
         MatToolbarModule,
         MatFormFieldModule, 
         MatInputModule, 
