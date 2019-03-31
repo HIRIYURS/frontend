58725abfe53c2decfa5d366a7ca10f8eb25a4e80
Santhosh Shankar <santhosh.shankar@gmail.com>
2019-03-31 14:32:21 -0500

    Made another fix to last test ci issue, checkin 8

--- src/app/components/edittask/edittask.component.spec.ts
+++ src/app/components/edittask/edittask.component.spec.ts
@@ -81,7 +81,7 @@ describe('EdittaskComponent', () => {
   });
 
   it('should create', () => {
-    //expect(component).toBeTruthy();
-    expect(1).toBe(1);
+    expect(component).toBeTruthy();
+    //expect(1).toBe(1);
   });
 });
