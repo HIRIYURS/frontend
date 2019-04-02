2786af875e163dbfb62da4e44b4493524ef80d02
Santhosh Shankar <santhosh.shankar@gmail.com>
2019-04-02 13:57:47 -0500

    Trying to fix Travis-ci test issue with EdittaskComponent - checkin 7

---
+++ src/app/components/edituser/edituser.component.spec.ts
@@ -0,0 +1,77 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { Router } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatToolbarModule, MatFormFieldModule, MatInputModule, MatOptionModule, 
+         MatSelectModule, MatIconModule, MatButtonModule, MatCardModule, 
+         MatTableModule, MatDividerModule, MatSnackBarModule, MatAutocompleteModule,
+         MatNativeDateModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
+import { EdituserComponent } from './edituser.component';
+import { ViewuserComponent } from '../viewuser/viewuser.component';
+
+import { UserService } from '../../user.service';
+
+const routes: Routes = [
+  { path: 'edituser', component: EdituserComponent},
+  { path: 'viewuser', component: ViewuserComponent},
+  { path: '', redirectTo: 'viewuser', pathMatch: 'full'}
+];
+
+describe('EdituserComponent', () => {
+  let component: EdituserComponent;
+  let fixture: ComponentFixture<EdituserComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        RouterModule.forRoot(routes),
+        FormsModule,
+        ReactiveFormsModule,
+        MatToolbarModule,
+        MatFormFieldModule, 
+        MatInputModule, 
+        MatOptionModule, 
+        MatSelectModule, 
+        MatIconModule, 
+        MatButtonModule, 
+        MatCardModule, 
+        MatTableModule, 
+        MatDividerModule, 
+        MatSnackBarModule,
+        MatSliderModule,
+        MatDatepickerModule,
+        MatAutocompleteModule,
+        MatNativeDateModule,
+        MatPaginatorModule,
+        MatSortModule   
+      ],
+      declarations: [ 
+        EdituserComponent,
+        ViewuserComponent
+      ],
+      providers: [ UserService ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EdituserComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
